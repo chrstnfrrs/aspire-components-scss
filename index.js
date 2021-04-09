047c0bad512e995ca73b8d89460b3061ce9e274a
Christian Farris <christianfarriscs@gmail.com>
2021-04-08 22:10:12 -0500

    add color and background color classes to export

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import './styles/justify.scss';
 import './styles/align.scss';
+import './styles/background-color.scss';
+import './styles/color.scss';
+import './styles/justify.scss';
+import './styles/md-background-color.scss';
+import './styles/md-color.scss';
 
 export default function noop() {}
