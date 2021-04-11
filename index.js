80c09cf0f9b991f8a1b85c5779d7b5f28b609719
Christian Farris <christianfarriscs@gmail.com>
2021-04-10 20:10:00 -0500

    export new display and direction classes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import './styles/align.scss';
 import './styles/background-color.scss';
 import './styles/color.scss';
+import './styles/direction.scss';
+import './styles/display.scss';
 import './styles/justify.scss';
 import './styles/md-background-color.scss';
 import './styles/md-color.scss';
