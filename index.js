25b350a5f2b9599714e20d4d52746b36ac0ced68
Christian Farris <christianfarriscs@gmail.com>
2021-04-16 19:36:22 -0500

    expose margin classes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import './styles/color.scss';
 import './styles/direction.scss';
 import './styles/display.scss';
 import './styles/justify.scss';
+import './styles/margin.scss';
 import './styles/md-background-color.scss';
 import './styles/md-color.scss';
 import './styles/padding.scss';
