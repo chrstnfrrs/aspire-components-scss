84c2efed4b95f9cce32402e617dad48349729faf
Christian Farris <christianfarriscs@gmail.com>
2021-05-14 22:42:51 -0500

    export elevation classes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import './styles/background-color.scss';
 import './styles/color.scss';
 import './styles/direction.scss';
 import './styles/display.scss';
+import './styles/md-elevation.scss';
 import './styles/height.scss';
 import './styles/justify.scss';
 import './styles/margin.scss';
